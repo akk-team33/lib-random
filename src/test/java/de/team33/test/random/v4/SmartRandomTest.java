730fe8d313308ea5daaced5dfcdf289946443e4a
andi <akk-pg@team33.de>
2019-05-04 17:38:05 +0200

    SmartRandom: test all primitives

--- src/test/java/de/team33/test/random/v4/SmartRandomTest.java
+++ src/test/java/de/team33/test/random/v4/SmartRandomTest.java
@@ -3,10 +3,14 @@ package de.team33.test.random.v4;
 import static java.lang.Boolean.TRUE;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 import de.team33.libs.random.v4.SmartRandom;
+import de.team33.libs.typing.v3.Type;
 import org.junit.Test;
 
 @SuppressWarnings("JUnitTestMethodWithNoAssertions")
@@ -37,7 +41,34 @@ public class SmartRandomTest {
 
     @Test
     public final void anyLong() {
-      assertEquals(LONG278, random.any(long.class));
-      assertEquals(LONG278, random.any(Long.class));
+        assertEquals(LONG278, random.any(long.class));
+        assertEquals(LONG278, random.any(Long.class));
+    }
+
+    @Test
+    public final void anyByType() {
+        final SmartRandom random = SmartRandom.builder().build();
+        for (final Type<?> type : Arrays.asList(
+                Type.of(boolean.class),
+                Type.of(Boolean.class),
+                Type.of(byte.class),
+                Type.of(Byte.class),
+                Type.of(short.class),
+                Type.of(Short.class),
+                Type.of(int.class),
+                Type.of(Integer.class),
+                Type.of(long.class),
+                Type.of(Long.class),
+                Type.of(float.class),
+                Type.of(Float.class),
+                Type.of(double.class),
+                Type.of(Double.class),
+                Type.of(char.class),
+                Type.of(Character.class)
+        )) {
+            final Object any = random.any(type);
+            assertNotNull(any);
+            assertTrue(type.getUnderlyingClass().isInstance(any));
+        }
     }
 }
