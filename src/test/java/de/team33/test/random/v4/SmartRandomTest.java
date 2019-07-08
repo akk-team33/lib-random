a215398b0e73f073fa5a05ce8e4d72a77f14232e
andi <akk-pg@team33.de>
2019-07-08 06:56:17 +0200

    new: MultiCase

--- src/test/java/de/team33/test/random/v4/SmartRandomTest.java
+++ src/test/java/de/team33/test/random/v4/SmartRandomTest.java
@@ -1,16 +1,17 @@
 package de.team33.test.random.v4;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import com.google.common.base.CaseFormat;
+import de.team33.libs.random.v4.SmartRandom;
+import de.team33.libs.random.v4.Typex;
+import org.junit.Test;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.base.CaseFormat;
-import de.team33.libs.random.v4.SmartRandom;
-import de.team33.libs.random.v4.Typex;
-import org.junit.Test;
+import static java.lang.String.format;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("JUnitTestMethodWithNoAssertions")
 public class SmartRandomTest {
@@ -19,42 +20,42 @@ public class SmartRandomTest {
     public final void anyByClass() {
         final SmartRandom random = SmartRandom.builder().build();
         for (final Class<?> type : Arrays.asList(
-          boolean.class, byte.class, short.class, int.class, long.class, float.class, double.class,
-          char.class, Boolean.class, Byte.class, Short.class, Integer.class, Long.class, Float.class,
-          Double.class, Character.class, TimeUnit.class, CaseFormat.class,
+                boolean.class, byte.class, short.class, int.class, long.class, float.class, double.class,
+                char.class, Boolean.class, Byte.class, Short.class, Integer.class, Long.class, Float.class,
+                Double.class, Character.class, TimeUnit.class, CaseFormat.class,
 
-          boolean[].class, byte[].class, short[].class, int[].class, long[].class, float[].class,
-          double[].class, char[].class, Boolean[].class, Byte[].class, Short[].class, Integer[].class,
-          Long[].class, Float[].class, Double[].class, Character[].class, TimeUnit[].class,
-          CaseFormat[].class
+                boolean[].class, byte[].class, short[].class, int[].class, long[].class, float[].class,
+                double[].class, char[].class, Boolean[].class, Byte[].class, Short[].class, Integer[].class,
+                Long[].class, Float[].class, Double[].class, Character[].class, TimeUnit[].class,
+                CaseFormat[].class
         )) {
             final Object any = random.any(type);
             assertType(type, any);
         }
     }
 
+    @Test
+    public final void anyByType() {
+        // TODO
+    }
+
     private void assertType(final Class<?> type, final Object value) {
-      assertType(type, type, value);
+        assertType(type, type, value);
     }
 
     private void assertType(final Class<?> context, final Class<?> type, final Object value) {
-      final String message =
-        "[" + context.getCanonicalName() + "] expected " + type.getCanonicalName() + " but was ";
-      assertNotNull(message + "null", value);
-      assertTrue(message + value.getClass().getCanonicalName(), Typex.isInstance(type, value));
-      if (value.getClass().isArray()) {
-          assertArrayElementsType(context, type, value);
-      }
+        final String message =
+                format("[%s] expected %s but was ", context.getCanonicalName(), type.getCanonicalName());
+        assertNotNull(message + "null", value);
+        assertTrue(message + value.getClass().getCanonicalName(), Typex.isInstance(type, value));
+        if (value.getClass().isArray()) {
+            assertArrayElementsType(context, type, value);
+        }
     }
 
-  private void assertArrayElementsType(final Class<?> context, final Class<?> type, final Object value)
-  {
-    for ( int index = 0; index < Array.getLength(value); ++index ) {
-        assertType(context, type.getComponentType(), Array.get(value, index));
-    }
-  }
-
-  @Test
-    public final void anyByType() {
+    private void assertArrayElementsType(final Class<?> context, final Class<?> type, final Object value) {
+        for (int index = 0; index < Array.getLength(value); ++index) {
+            assertType(context, type.getComponentType(), Array.get(value, index));
+        }
     }
 }
