784cab4c3a29481571c9f8aa2fb660db9d32c713
KlugeKaindl <akk-gthb@team33.de>
2019-11-26 16:40:41 +0100

    new: Assert

---
+++ src/test/java/de/team33/test/random/v5/Assert.java
@@ -0,0 +1,35 @@
+package de.team33.test.random.v5;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class Assert<T> {
+
+    public static final Function<Object, String> DOUBLE = subject -> Double.class.isInstance(subject) ? null : "\n" +
+            "expected: Double\n" +
+            " but was: " + (null == subject ? null : subject.getClass());
+    public static final Function<Object, String> NULL = subject -> null == subject ? null : "\n" +
+            "expected: null\n" +
+            " but was: " + subject;
+    public static final Function<Object, String> NOT_NULL = subject -> null != subject ? null : "\n" +
+            "expected: non-null value\n" +
+            " but was: null";
+
+    private final T subject;
+
+    private Assert(final T subject) {
+        this.subject = subject;
+    }
+
+    public static <T> Assert<T> that(final T subject) {
+        return new Assert<>(subject);
+    }
+
+    public final Assert<T> is(final Function<? super T, String> matcher) {
+        Optional.ofNullable(matcher.apply(subject))
+                .ifPresent(message -> {
+                    throw new AssertionError(message);
+                });
+        return this;
+    }
+}
---
+++ src/test/java/de/team33/test/random/v5/AssertTest.java
@@ -0,0 +1,25 @@
+package de.team33.test.random.v5;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static de.team33.test.random.v5.Assert.DOUBLE;
+import static de.team33.test.random.v5.Assert.NOT_NULL;
+import static de.team33.test.random.v5.Assert.NULL;
+
+public class AssertTest {
+
+    @Test
+    public void thatNull() {
+        Assert.that(null)
+              .is(NULL);
+    }
+
+    @Test
+    public void that() {
+        Assert.that(new Random().nextDouble())
+              .is(NOT_NULL)
+              .is(DOUBLE);
+    }
+}
